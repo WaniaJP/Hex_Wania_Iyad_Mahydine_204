5457b120ae86d72f067baa76afce3c062f3c958b
Iyad00 <31134802+Iyad00@users.noreply.github.com>
2023-01-02 23:41:48 +0100

    PionTest

---
+++ test/java/plateau/PionTest.java
@@ -0,0 +1,28 @@
+package test.java.plateau;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import main.java.plateau.Pion;
+
+class PionTest {
+
+	@Test
+	void test() {
+		assertEquals("X",Pion.Croix.toString());
+		assertEquals("O",Pion.Rond.toString());
+		assertEquals(".",Pion.Vide.toString());
+		
+		assertTrue(Pion.Croix == Pion.get('X'));
+		assertTrue(Pion.Rond == Pion.get('O'));
+		assertTrue(Pion.Vide == Pion.get('.'));
+		
+		assertThrows(IllegalArgumentException.class,
+				() -> {
+					Pion.get('*');
+				}
+				);
+	}
+
+}
